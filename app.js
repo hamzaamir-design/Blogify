9e1ecbf1503580e1cfd32d46016015b71ea27f5e
hamzaamir-design <hamzaamir9733@gmail.com>
2025-09-30 04:08:46 +0500

    add Commit

--- app.js
+++ app.js
@@ -9,32 +9,32 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./MiddleWares/authentication");
 const Blog = require('./models/blog');
-const { connected } = require("process");
+// const { connected } = require("process");
 
 const PORT = process.env.PORT || 8000;
 
-// mongoose
-//     // .connect(process.env.Mongo_URL)
-//     .connect('mongodb://localhost:27017/Blogify')
-//     .then((e) => console.log("MongoDB Connected"));
+mongoose
+    // .connect(process.env.Mongo_URL)
+    .connect('mongodb://localhost:27017/Blogify')
+    .then((e) => console.log("MongoDB Connected"));
 
-let isConnected = false;
-async function connectToMongoDb() {
-    try {
-        await mongoose.connect(process.env.Mongo_URL);
-        isConnected = true;
-        console.log("Connected to mongodb")
-    } catch (error) {
-        console.error('Error connecting to MongoDb: ', error);
-    }
-}
+// let isConnected = false;
+// async function connectToMongoDb() {
+//     try {
+//         await mongoose.connect(process.env.Mongo_URL);
+//         isConnected = true;
+//         console.log("Connected to mongodb")
+//     } catch (error) {
+//         console.error('Error connecting to MongoDb: ', error);
+//     }
+// }
 
-app.use((req, res, next) => {
-    if (!isConnected) {
-        connectToMongoDb();
-    }
-    next();
-})
+// app.use((req, res, next) => {
+//     if (!isConnected) {
+//         connectToMongoDb();
+//     }
+//     next();
+// })
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -57,6 +57,6 @@ app.get('/', async (req, res) => {
 
 });
 
-// app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
 
-module.exports = app;
+// module.exports = app;
